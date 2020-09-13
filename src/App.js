1acc03a4cef8e94f61b4d0d39b75783197fcc060
Andrew Reynolds <andrew_reynolds@mac.com>
2020-09-13 13:01:02 -0700

    created nav

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React, { Component } from "react";
+import Navbar from "./components/layout/Navbar";
 import "./App.css";
 
 class App extends Component {
   render() {
-    const name = "John Doe";
     return (
       <div className="App">
-        <h1>Hello {name}</h1>
+        <Navbar />
       </div>
     );
   }
